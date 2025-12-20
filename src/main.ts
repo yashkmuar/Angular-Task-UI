77060a1d816d08d03ecb86680bc8d44c51764cfc
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-12-20 22:01:21 +0530

    user component added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HeaderComponent } from './app/header.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));

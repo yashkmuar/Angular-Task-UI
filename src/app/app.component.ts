90c5b6142950eae5307cdb9ec097c2001f4c7a22
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-12-30 16:44:50 +0530

    avatar property called from app component, DUMMY_USERS inherited in app component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS }  from './dummy-users';
 
 @Component({
   selector: 'app-root',

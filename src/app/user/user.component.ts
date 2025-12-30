6cfb6ccc8dc52ffac78d781e729cd74d583ce4fe
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-12-30 17:09:05 +0530

    now I make sure that all input variables in user comonent is required while initializing

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,14 +9,13 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!:string;
-  @Input() name!:string;
+  @Input({required: true}) avatar!:string;
+  @Input({required:true}) name!:string;
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
   }
 
   onSelectUser(){
-
   }
 }

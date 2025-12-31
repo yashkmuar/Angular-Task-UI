e4138203cfa506634780300c7b24d26a6949eb7d
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-12-31 11:01:16 +0530

    reversing changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,8 +12,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  // @Output() select = new EventEmitter();
-  select = output<string>();
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;

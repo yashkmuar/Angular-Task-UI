c61f0d05bb89d9f44278f508b84a9ac05759ca39
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-12-31 10:57:36 +0530

    doing emittion using ouput() function instead of output() decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, Input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, Input, Output, output } from '@angular/core';
 
 
 @Component({
@@ -12,7 +12,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;

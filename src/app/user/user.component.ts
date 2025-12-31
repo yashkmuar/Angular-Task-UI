5ecc9c0112e56e16cb808d5d8915d33eaccb83f7
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-12-31 10:25:45 +0530

    uses of output emit in user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, Input, Output } from '@angular/core';
 
 
 @Component({
@@ -9,13 +9,16 @@ import { Component, computed, Input, Output } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required:true}) id!:string;
   @Input({required: true}) avatar!:string;
   @Input({required:true}) name!:string;
+  @Output() select = new EventEmitter();
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
   }
 
   onSelectUser(){
+    this.select.emit();
   }
 }

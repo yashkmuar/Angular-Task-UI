f0c5a5b6cab6e47ba62b0d546e47fe86e68c581c
Yash Kumar <jitendrakumar1988w@gmail.com>
2026-01-02 23:32:42 +0530

    to see the tasks of user while clicking, not preset, I use the if and else statement for that

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
-import { DUMMY_USERS }  from './dummy-users';
+import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
@@ -12,14 +12,14 @@ import { TasksComponent } from './tasks/tasks.component';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
- users = DUMMY_USERS;
- selectedUserId: string = 'u1';
+  users = DUMMY_USERS;
+  selectedUserId?: string;
 
- get selectedUser(){
-  return this.users.find((user) => user.id === this.selectedUserId);
- }
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 
- onSelectUser(id:string){
-  this.selectedUserId = id;
- }
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }

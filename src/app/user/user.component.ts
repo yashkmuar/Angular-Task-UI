bfdc11f089d47af8e5a9936d7927db72d4076664
Yash Kumar <jitendrakumar1988w@gmail.com>
2026-01-02 22:44:00 +0530

    tasks component added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,7 +12,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;

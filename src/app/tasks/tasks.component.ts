533439559084fe828e64d71e48976f3d5672dec1
Yash Kumar <jitendrakumar1988w@gmail.com>
2026-01-03 13:39:49 +0530

    whenever new task added it reflected on the UI page

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  isAddingTask = false;
 
   tasks = [
     {
@@ -47,6 +49,6 @@ export class TasksComponent {
   }
 
   onStartAddTask(){
-    console.log('Task Add clicked');
+    this.isAddingTask = true;
   }
 }

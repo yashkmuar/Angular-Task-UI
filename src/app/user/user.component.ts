8e54db011b0657a217d8eb66ad34e054936e18a7
Yash Kumar <jitendrakumar1988w@gmail.com>
2026-01-03 12:49:06 +0530

    The selected user gets remain colored, while selecting the tasks

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,6 +10,7 @@ import { User } from './user.model';
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({required: true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {

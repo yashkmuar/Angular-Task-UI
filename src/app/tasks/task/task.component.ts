774caec5e2cc60c3535a1642f9d6f65099f18d7b
Yash Kumar <jitendrakumar1988w@gmail.com>
2026-01-03 12:59:19 +0530

    whenever the user complete the task, it should be reflected on the tasks component

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './task.model';
 
 
@@ -11,4 +11,9 @@ import { Task } from './task.model';
 })
 export class TaskComponent {
   @Input({required: true}) task!: Task;
+  @Output() complete = new EventEmitter();
+
+  onCompleteTask(){
+    this.complete.emit(this.task.id);
+  }
 }

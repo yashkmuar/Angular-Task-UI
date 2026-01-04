3e2a1787cf6d74e8376fec4f00fd65cb4fb59cf3
Yash Kumar <jitendrakumar1988w@gmail.com>
2026-01-04 11:21:37 +0530

    make one shared folder for rounded edges and shadow for css, and use it with user and task component by using ng-content

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,11 @@
 import { Component, computed, EventEmitter, Input, Output, output } from '@angular/core';
 import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })

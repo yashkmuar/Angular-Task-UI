6caf29fc17df37b3f4ee99b8afb5fd4a82bc5912
Yash Kumar <jitendrakumar1988w@gmail.com>
2026-01-04 13:46:17 +0530

    so, now whatever data we get is now stored in local data storage, and because of that my tasks are now in updated state, does not get loose when everytime refresh

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -30,6 +30,14 @@ export class TasksService {
         }
     ];
 
+    constructor(){
+        const tasks = localStorage.getItem('tasks');
+
+        if(tasks){
+            this.tasks = JSON.parse(tasks);
+        }
+    }
+
     getUserTasks(userId: string) {
         return this.tasks.filter((task) => task.userId === userId);
     }
@@ -41,11 +49,16 @@ export class TasksService {
             title: taskData.title,
             summary: taskData.summary,
             dueDate: taskData.dueDate,
-        })
+        });
+        this.saveTasks();
     }
 
     removeTask(id: string) {
         this.tasks = this.tasks.filter((task) => task.id !== id);
+        this.saveTasks();
     }
 
+    private saveTasks(){
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
 }
